3a9cdcc23eaa751b4aacdea4a55b3f27277cd9be
AndreasPatsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2020-03-09 21:27:30 +0200

    Update EmployeeController.java

--- src/main/java/org/patsimas/solr/controllers/EmployeeController.java
+++ src/main/java/org/patsimas/solr/controllers/EmployeeController.java
@@ -3,9 +3,8 @@ package org.patsimas.solr.controllers;
 import org.patsimas.solr.domain.Employee;
 import org.patsimas.solr.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -63,5 +62,13 @@ public class EmployeeController {
     public List<Employee> findByName(@PathVariable String name){
 
         return employeeRepository.findByNameContaining(name);
+    }
+
+    @PostMapping(value = "employees")
+    public ResponseEntity<?> saveEmployee(@RequestBody Employee employee){
+
+        employeeRepository.save(employee);
+
+        return ResponseEntity.ok("SAVED");
     }
 }
